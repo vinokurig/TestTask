1097a0ec83659a6269e65b3e07a3af0922fb32be
vinokurig <vinokurig@gmail.com>
2014-07-07 12:19:53 +0300

    Project for mavenn

---
+++ src/main/java/com/codenvy/testtask/qname/IllegalNameException.java
@@ -0,0 +1,10 @@
+package com.codenvy.testtask.qname;
+
+/**
+ * Thrown to show that the name is illegal.
+ * 
+ * @version		1.1     07 July 2014
+ * @author 		Vinokur Igor
+ */
+public class IllegalNameException extends Exception {
+} 
